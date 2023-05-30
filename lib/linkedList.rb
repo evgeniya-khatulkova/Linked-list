b6372d2ca0234f3bf2c60a698789f0d242ac6bbd
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-05-30 14:11:14 +0200

    create append and prepend functions for the node list

--- lib/linkedList.rb
+++ lib/linkedList.rb
@@ -1,3 +1,30 @@
 class LinkedList
-  
+  attr_accessor :head
+
+  def initialize
+    @head = Node.new
+  end
+
+  def append(value)
+    if @head.value == nil
+      @head.value = value
+    else
+      current_node = @head
+      new_node = Node.new(value)
+      until current_node.next_node.nil?
+        current_node = current_node.next_node
+      end
+      current_node.next_node = new_node
+    end
+  end
+
+  def prepend(value)
+    if @head.value.nil?
+      @head.value = value
+    else
+      new_node = Node.new(value)
+      new_node.next_node = @head
+      @head = new_node
+    end
+  end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,14 @@
 require_relative 'node'
 require_relative 'linkedList'
 
-p Node.new
-p LinkedList.new
+linked = LinkedList.new
+# p linked.head
+
+linked.prepend("cool")
+# p linked
+linked.prepend("yo")
+# p linked
+
+# p linked
+linked.append("end")
+p linked
